ee0b841ad8515ade3e191ddd4c908dedb710cb2e
Dayana Alejandro <dayana200120@gmail.com>
2021-07-09 18:21:19 +0000

    Updating
    Co-authored-by: Lily Sam <lilyoforiwaas@gmail.com>

--- safety.py
+++ safety.py
@@ -4,10 +4,13 @@ def start(country_name):
     with open(fileName,"r") as file:
         for line in file: 
             line = line.split()
-            country_info[line[1]] = {"Safety Rank": line[0],"Safety Index Score":line[2]}
+            length = len(line)
+            name = ' '.join(line[1:length-1])
+            country_info[name] = {"Safety Rank": line[0],"Safety Index Score":line[-1]}
 
     for key, value in country_info[country_name].items():
         print(key, " : ", value)
+    
 
 def main():
     country_name = input("What country? (Full Name Please) ")
--- script.py
+++ script.py
@@ -1,7 +1,9 @@
+import pycountry
 from weather import *
 from holidays import *
 from safety import *
 from translate import Translator #sudo pip3 install translate
+import pycountry # sudo pip3 install pycountry
 
 def print_header(title):
     print(f'\n{title}')
@@ -33,37 +35,59 @@ def weather(lat, lon, location, city_name):
     tableName = 'weather_table'
     df, engine = update_w(dbName, fileName, tableName, info)
     write_table_w(dbName,engine, info)
-    save_data_w(dbName, fileName, engine,info)
+    # save_data_w(dbName, fileName, engine,info)
     # visualiation (NEEDS IMPROVEMENT)
     df.plot.bar(x='date', y='temp')
+    plt.ylabel('°F')
     plt.title(location.address)
-    plt.savefig('weather_bar.png')
+    plt.savefig('weather_bar.png', bbox_inches = 'tight')
     
     
-def holidays(country_name):
+def holidays(country_name):   
     # holiday main
-    database = 'holidays'
-    file = 'holiday_file'
-    table = 'holiday_table'
-    engine_h = create_engine('mysql://root:codio@localhost/' + database)
     api_key = get_api()
     url = build_url(country_name, api_key) # moved this up here
-    dic = build_dict(url) # added this
-    holiday_df = build_dataframe(dic) #added this
-    df = update_database(database, table, file, engine_h)    
-    write_table_h(df, database, table, engine_h)
-    save_data_to_file(df, database, table, file, engine_h)
+    json = get_json(url)
+    dic = build_dict(json)
+    df = build_dataframe(dic)
+    print(df)
+#     database = 'holidays'
+#     file = 'holiday_file'
+#     table = 'holiday_table'
+#     engine_h = create_engine('mysql://root:codio@localhost/' + database)
+#     api_key = get_api()
+#     url = build_url(country_name, api_key) # moved this up here
+#     json = get_json(url)
+#     print(json)
+#     #dic = build_dict(json) # added this
+# #     holiday_df = build_dataframe(dic) #added this
+# #     df = update_database(database, table, file, engine_h)    
+# #     write_table_h(df, database, table, engine_h)
+# #     save_data_to_file(df, database, table, file, engine_h)
     
-def safety(translator, destination_name):
-    translation = translator.translate(destination_name)
-    start(translation)
+def safety(destination_name):
+    #translation = translator.translate(destination_name)
+    start(destination_name)
     
+def convert_name_alpha3(country_name):
+    countries = {}
+    if (country_name == 'United Kingdom'):
+        code = 'UK'
+    elif(country_name == 'United States'):
+        code = 'USA'
+    else:
+        for country in pycountry.countries:
+            countries[country.name] = country.alpha_2
+            code = countries.get(country_name, 'Unknown code')
+
+    return code  # prints ['AS', 'CA', 'FR']
+
     
 def main():
-    language = input("Input Language format for city and country name: ")
+    language = input("Input Language For City: ")
     translator= Translator(from_lang=language,to_lang="english")
     # weather main
-    destination_name = input("City and/or State/Country of Destination: ")
+    destination_name = input("City and State/Country of Destination: ")
     lat, lon, location = get_data_w(destination_name)
     #holiday
     country_name = location.address.split(', ')[-1]
@@ -78,10 +102,21 @@ def main():
             weather(lat, lon, location, destination_name)
         elif(option==2):
             print_header("Holidays for "+ location.address)
-            holidays(country_name)
-        elif(option==3):
+            country_code = convert_name_alpha3(country_name)
+            holidays(country_code)
+#             try:
+#                 country_code = convert_name_alpha3(country_name)
+#                 holidays(country_code)
+#             except IOError:
+#                 print ('trying different format', arg)
+#             else:
+#                 country_code = convert_name_alpha2(country_name)
+#                 holidays(country_code)
+        elif(option==3):   
             print_header("Safety for "+ location.address)
-            safety(translator, country_name)
+            print_header("Safety for "+ location.address)
+            translation = translator.translate(country_name)
+            safety(translation)
         else:
             print('\nInvalid choice, select another option')
             
